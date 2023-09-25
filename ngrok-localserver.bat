ebefc6fde0f40866123ab37e30bee720a0ad9340
J0nathan550 <ihudokormov@gmail.com>
2023-09-25 16:34:11 +0200

    added all files to show example

---
+++ ngrok-localserver.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Minecraft Server managing using Ngrok by J0nathan550
+ngrok.exe
+ngrok tcp --region eu 25565
---
+++ start-server.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Minecraft Server preparing to run, by J0nathan550
+start ngrok-localserver.bat
+start minecraft-localserver.bat
+quit
